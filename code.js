24a306beca810fd7c7b6ec3522d5b234c4262f37
napat-s3invent <napat.p@s3invent.com>
2021-01-27 22:36:11 +0700

    init

---
+++ code.js
@@ -0,0 +1,31 @@
+const https = require("https");
+//const jsdom = require("jsdom"); if can use Third party module
+const option = {
+  host: "codequiz.azurewebsites.net",
+  headers: {
+    Cookie: "hasCookie=true",
+  },
+};
+const callback = (response) => {
+  response.setEncoding("utf8");
+  response.on("data", function (chunk) {
+    chunk
+      .split("tr")
+      .filter((ele) => ele.includes("td"))
+      .forEach((node) => {
+        if (node.includes(process.argv[2])) {
+          console.log(node.split("</td><td>")[1]);
+        }
+      });
+    // if can use Third party module
+    // const dom = new jsdom.JSDOM(chunk);
+    // dom.window.document.querySelectorAll("tr").forEach((node) => {
+    //   if (node.innerHTML.includes(process.argv[2])) {
+    //     console.log(node.querySelectorAll("td")[1].innerHTML);
+    //   }
+    // });
+    //
+  });
+  response.on("end", function () {});
+};
+https.request(option, callback).end();
